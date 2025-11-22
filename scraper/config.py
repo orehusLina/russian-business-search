8d98826bb810bf229e2205c0d5e50fda0e2b7489
Alina Orekhova <40996928+orehusLina@users.noreply.github.com>
2025-11-22 12:42:00 +0400

    Update config.py
    changes some things

--- scraper/config.py
+++ scraper/config.py
@@ -1,26 +1,18 @@
-"""
-Конфигурация и константы скрапера
-"""
-
 BASE_URL = "https://rb.ru"
 
 SECTIONS = {
     'news': '/news/',
     'stories': '/stories/',
-    'opinions': '/columns/',  # Мнения находятся по адресу /columns/
+    'opinions': '/columns/',
     'neuroprofiles': '/neuro/',
     'reviews': '/reviews/',
     'checklists': '/checklists/'
 }
 
-# Настройки по умолчанию
-DEFAULT_MAX_WORKERS = 20  # Увеличено для максимальной параллельности
-DEFAULT_DELAY = 0.3  # Уменьшено для скорости
-DEFAULT_TIMEOUT = 15  # Увеличено для медленных соединений
-DEFAULT_RETRIES = 5  # Увеличено количество попыток
+DEFAULT_MAX_WORKERS = 20
+DEFAULT_DELAY = 0.3
+DEFAULT_TIMEOUT = 15
+DEFAULT_RETRIES = 5
 
-# Регулярные выражения для поиска статей
-# Включаем columns для мнений
 ARTICLE_URL_PATTERN = r'/(news|stories|columns|opinions|neuroprofiles|reviews|checklists)/[^/?]+/?$'
 FULL_URL_PATTERN = r'https?://(?:www\.)?rb\.ru/(news|stories|columns|opinions|neuroprofiles|reviews|checklists)/[^/?]+/?$'
-
